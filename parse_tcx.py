0b359c90f8970fc08716447798b682e369fbbb71
Stefan Winkler <mail@stefanwinkler.de>
2019-06-03 19:14:58 +0200

    Convert tcx files to protobuf

--- parse_tcx.py
+++ parse_tcx.py
@@ -1,28 +1,108 @@
 import xml.dom.minidom
 import dateutil.parser
 import datetime
+import time
+import tcx_pb2
+import glob
+import os
+
+def getChildByTagName(node, name):
+    return list(filter(lambda n: n.nodeName == name, node.childNodes))
+
+def getStringValue(parentNode, name):
+    tagNodeList = getChildByTagName(parentNode, name)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return node.childNodes[0].nodeValue
+    else:
+        return ""
+
+
+def getFloatValue(parentNode, name):
+    tagNodeList = getChildByTagName(parentNode, name)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return float(node.childNodes[0].nodeValue)
+    else:
+        return 0.0
+
+
+def getIntValue(parentNode, name):
+    tagNodeList = getChildByTagName(parentNode, name)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return int(node.childNodes[0].nodeValue)
+    else:
+        return 0
+
+
+def getBoolValue(parentNode, name, trueString="True"):
+    tagNodeList = getChildByTagName(parentNode, name)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return node.childNodes[0].nodeValue == trueString
+    else:
+        return False
+
+
+def getTimeValue(parentNode, name, trueString="True"):
+    tagNodeList = getChildByTagName(parentNode, name)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return dateutil.parser.parse(node.childNodes[0].nodeValue)
+    else:
+        return None
+
+
+def getNestedIntValue(parentNode, nested, name):
+    tagNodeList = getChildByTagName(parentNode, nested)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return getIntValue(node, name)
+    else:
+        return 0
+
+
+def getNestedFloatValue(parentNode, nested, name):
+    tagNodeList = getChildByTagName(parentNode, nested)
+    if (len(tagNodeList) == 1):
+        node = tagNodeList[0]
+        return getFloatValue(node, name)
+    else:
+        return 0.0
+
 
 class TrackPoint:
-    def __init__(self, trackPointNode = None):
+    def __init__(self, trackPointNode=None):
         self.distance = 0.0
         self.time = None
         self.velocity = 0.0
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.altitude = 0.0
+        self.heartrate = 0.0
+        self.cadence = 0.0
+        self.sensorPresent = False
         if (trackPointNode == None):
             return
-        distanceMetersNodeList = trackPointNode.getElementsByTagName(
-            "DistanceMeters")
-        if (len(distanceMetersNodeList) != 1):
-            return None
-        distanceMetersNode = distanceMetersNodeList[0]
-        distanceText = distanceMetersNode.childNodes[0].nodeValue
-        self.distance = float(distanceText)
+        # Distance
+        self.distance = getFloatValue(trackPointNode, "DistanceMeters")
         # Time
-        timeNodeList = trackPointNode.getElementsByTagName("Time")
-        if (len(timeNodeList) != 1):
-            return None
-        timeNode = timeNodeList[0]
-        timeText = timeNode.childNodes[0].nodeValue
-        self.time = dateutil.parser.parse(timeText)
+        self.time = getTimeValue(trackPointNode, "Time")
+        # Position
+        self.latitude = getNestedFloatValue(
+            trackPointNode, "Position", "LatitudeDegrees")
+        self.longitude = getNestedFloatValue(
+            trackPointNode, "Position", "LongitudeDegrees")
+        # altitude
+        self.altitude = getFloatValue(trackPointNode, "AltitudeMeters")
+        # heartRate
+        self.heartrate = getNestedIntValue(trackPointNode, "HeartRateBpm", "Value")
+        # cadence
+        self.cadence = getIntValue(trackPointNode, "Cadence")
+        # SensorPresent
+        self.sensorPresent = getBoolValue(
+            trackPointNode, "SensorState", "Present")
 
     def __str__(self):
         return "Distance: {} (Velocity: {} Time: {})\n".format(self.distance, self.velocity, str(self.time))
@@ -33,9 +113,20 @@ class TrackPoint:
     def timeTo(self, other):
         return (other.time - self.time).total_seconds()
 
+    def to_pb2(self):
+        tp = tcx_pb2.TrackPoint()
+        tp.distanceMeters = self.distance
+        tp.time = int(time.mktime(self.time.timetuple()))
+        tp.position.latitudeDegrees = self.latitude
+        tp.position.longitudeDegrees = self.longitude
+        tp.altitudeMeters = self.altitude
+        tp.cadence = self.cadence
+        tp.sensorPresent = self.sensorPresent
+        return tp
+
 
 class Track:
-    def __init__(self, trackNode = None):
+    def __init__(self, trackNode=None):
         self.trackpoints = []
         if (trackNode == None):
             return
@@ -55,12 +146,43 @@ class Track:
             serialized = serialized + str(tp)
         return serialized
 
+    def to_pb2(self):
+        track = tcx_pb2.Track()
+        track.trackpoint.extend(
+            list(map(lambda tp: tp.to_pb2(), self.trackpoints)))
+        return track
+
 
 class Lap:
-    def __init__(self, lapNode = None):
+    def __init__(self, lapNode=None):
         self.tracks = []
+        self.totalTimeSeconds = 0.0
+        self.distanceMeters = 0.0
+        self.maximumSpeed = 0.0
+        self.calories = 0.0
+        self.averageHeartRateBPM = 0
+        self.maximumHeartRateBPM = 0
+        self.intensityActive = False
+        self.cadence = 0
+        self.triggerMethod = "Manual"
+        self.notes = ""
+        self.startTime = None
         if (lapNode == None):
             return
+        self.sport = dateutil.parser.parse(lapNode.attributes["StartTime"].nodeValue)
+        self.totalTimeSeconds = getFloatValue(lapNode, "TotalTimeSeconds")
+        self.distanceMeters = getFloatValue(lapNode, "DistanceMeters")
+        self.maximumSpeed = getFloatValue(lapNode, "MaximumSpeed")
+        self.calories = getIntValue(lapNode, "Calories")
+        self.averageHeartRateBPM = getNestedIntValue(
+            lapNode, "AverageHeartRateBpm", "Value")
+        self.maximumHeartRateBPM = getNestedIntValue(
+            lapNode, "MaximumHeartRateBpm", "Value")
+        self.intensityActive = getBoolValue(lapNode, "Intensity", "Active")
+        self.cadence = getIntValue(lapNode, "Cadence")
+        self.triggerMethod = getStringValue(lapNode, "TriggerMethod")
+        self.notes = getStringValue(lapNode, "Notes")
+
         for lapChild in lapNode.childNodes:
             if (lapChild.nodeName == "Track"):
                 track = Track(lapChild)
@@ -78,15 +200,40 @@ class Lap:
             allTp.extend(t.trackpoints)
         return allTp
 
+    def to_pb2(self):
+        lap = tcx_pb2.ActivityLap()
+        lap.track.extend(list(map(lambda t: t.to_pb2(), self.tracks)))
+        lap.totalTimeSeconds = self.totalTimeSeconds
+        lap.distanceMeters = self.distanceMeters
+        lap.maximumSpeed = self.maximumSpeed
+        lap.calories = self.calories
+        lap.averageHeartRateBPM = self.averageHeartRateBPM
+        lap.maximumHeartRateBPM = self.maximumHeartRateBPM
+        lap.intensityActive = self.intensityActive
+        lap.cadence = self.cadence
+        lap.notes = self.notes
+        tmMap = {"Manual": tcx_pb2.ActivityLap.MANUAL,
+                 "Distance": tcx_pb2.ActivityLap.DISTANCE,
+                 "Location": tcx_pb2.ActivityLap.LOCATION,
+                 "Time": tcx_pb2.ActivityLap.TIME,
+                 "HeartRate": tcx_pb2.ActivityLap.HEARTRATE
+                 }
+        lap.triggerMethod = tmMap[self.triggerMethod]
+        return lap
+
 
 class Activity:
     span_distances = [400, 1000, 2000, 3000,
                       5000, 10000, 20000, 21097.5, 42193]
 
-    def __init__(self, activityNode = None):
+    def __init__(self, activityNode=None):
         self.sport = "Running"
         self.laps = []
         self.bestSpans = []
+        self.id = None
+        self.notes = ""
+        self.training = None
+        self.creator = ""
         if (activityNode == None):
             return
         self.sport = activityNode.attributes["Sport"].nodeValue
@@ -95,6 +242,7 @@ class Activity:
                 continue
             lap = Lap(activityChild)
             self.laps.append(lap)
+        self.id = getTimeValue(activityNode, "Id")
         self.addVelocities()
         self.addBestSpans()
 
@@ -136,6 +284,8 @@ class Activity:
         allTp = self.allTrackPoints()
         span = [0, 1]
         best_time = -1
+        if len(allTp) < 1:
+            return best_time
         if allTp[0].distanceTo(allTp[-1]) < distance:
             return best_time
         while True:
@@ -162,6 +312,11 @@ class Activity:
             if time != -1:
                 self.bestSpans.append(time)
 
+    def to_pb2(self):
+        act = tcx_pb2.Activity()
+        act.lap.extend(list(map(lambda l: l.to_pb2(), self.laps)))
+        return act
+
 
 class TrainingCenterDatabase:
     def __init__(self, filename):
@@ -182,6 +337,31 @@ class TrainingCenterDatabase:
             serialized = serialized + str(act)
         return serialized
 
-tcd = TrainingCenterDatabase(
-    './tcxdata/2019-05-28T11_45_53.000Z_3528649242.tcx')
-print(tcd)
+    def to_pb2(self):
+        tcd = tcx_pb2.TrainingCenterDatabase()
+        tcd.activities.activity.extend(
+            list(map(lambda a: a.to_pb2(), self.activities)))
+        return tcd
+
+
+#tcd = TrainingCenterDatabase(
+#    './tcxdata/2019-05-28T11_45_53.000Z_3528649242.tcx')
+
+#tcd_proto = tcd.to_pb2()
+#f = open("tcx.pb", "wb")
+#f.write(tcd_proto.SerializeToString())
+#f.close()
+
+#print(tcd)
+
+tcx_files = glob.glob('./tcxdata/*.tcx')
+if not os.path.exists('pbdata'):
+    os.makedirs('pbdata')
+for file_name in tcx_files:
+    base_name = os.path.basename(file_name)
+    root, ext = os.path.splitext(base_name)
+    print(file_name)
+    tcd = TrainingCenterDatabase(file_name)
+    f = open("pbdata/{}.pb".format(root), "wb")
+    f.write(tcd.to_pb2().SerializeToString())
+    f.close()
